66813de6af70a74bb53e0e8d1ee052d5cf2c70f0
wilnzi <wilnzi@yahoo.fr>
2020-02-10 17:35:20 +0100

    Add files via upload

---
+++ database/src/main/java/cm/wilnzi/database/DataBaseConnectionManager.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cm.wilnzi.database;
+
+/**
+ *
+ * @author WILFRIED
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnectionManager {
+
+    private Connection connection = null;
+
+    static String DATABASE_PASSWORD = "eGN4e$$GN3eEBAISFjMlZoWkcxcGNCWJrQjNhV3h1ZW1r@@dVkyMGdZMjl2YlE";
+    static int DATABASE_PORT = 3306;
+    String DATABASE_PSEUDO = "seadmin";
+
+    public Connection openConnection() {
+
+//        int DATABASE_PORT = 5462;
+        String DATABASE_URL = "jdbc:mariadb://localhost:" + DATABASE_PORT + "/sedb01?allowMultiQueries=true&characterEncoding=UTF-8&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            if (this.connection == null) {
+                this.connection = DriverManager.getConnection(DATABASE_URL, DATABASE_PSEUDO, DATABASE_PASSWORD);
+                this.connection.setAutoCommit(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return this.connection;
+    }
+
+    public Connection openConnection(boolean autoCommit) {
+        try {
+            this.connection = openConnection();
+            this.connection.setAutoCommit(autoCommit);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return this.connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+            if (this.connection != null) {
+                this.connection = null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
